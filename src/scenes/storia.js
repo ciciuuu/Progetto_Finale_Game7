cc103a1abb8b9a7c1cd4a0ae26e8664d11025f8f
Edoardo Cicerone <edoardofederico.cicerone@polimi.it>
2025-12-09 12:18:04 +0100

    prove scena storie

--- src/scenes/storia.js
+++ src/scenes/storia.js
@@ -1,45 +1,38 @@
-let sfondi = [];
-let current_bg;
-let bg_index = 0;
+let sfondi_storia
+let tavola1 
+let testo1 
+let tavola2 
+let testo2 
+let testo3
+let tavola_attiva
+let tavolalunga
 
 function preload(s) {
   // Carico i 6 sfondi
-  sfondi[0] = PP.assets.image.load(s, "assets/images/tavola1.jpeg");
-  sfondi[2] = PP.assets.image.load(s, "assets/images/testo1.pdf");
-  sfondi[1] = PP.assets.image.load(s, "assets/images/tavola2.jpeg");
-  sfondi[3] = PP.assets.image.load(s, "assets/images/testo2.pdf");
-  sfondi[4] = PP.assets.image.load(s, "assets/images/testo3.pdf");
+  tavola1 = PP.assets.image.load(s, "assets/images/tavola1.jpeg");
+  testo1 = PP.assets.image.load(s, "assets/images/testo1.pdf");
+  tavola2 = PP.assets.image.load(s, "assets/images/tavola2.jpeg");
+  testo2 = PP.assets.image.load(s, "assets/images/testo2.pdf");
+  testo3 = PP.assets.image.load(s, "assets/images/testo3.pdf");
+  tavolalunga = PP.assets.image.load(s, "assets/images/tavole_storia.jpg");
 }
 
 function create(s) {
   // Mostro il primo sfondo
-  current_bg = PP.assets.image.add(s, sfondi[bg_index], 0, 0, 0, 0);
-
-  PP.shapes.text_styled_add(s, 
-    PP.game.config.canvas_width / 2,
-    PP.game.config.canvas_height / 2,
-    "Crediti: premi M per tornare al menu",
-    50,
-    "Helvetica",
-    "normal",
-    "0xFFFFFF",
-    null,
-    0.5,
-    0.5);
+  
+  let tavola_attiva = PP.assets.image.add(s, tavolalunga, 0, 0, 0, 0);
+   
+    
 }
 
 function update(s) {
+
   if(PP.interactive.kb.is_key_down(s, PP.key_codes.M)) {
     PP.scenes.start("main_menu");
   }
 
   if(PP.interactive.kb.is_key_down(s, PP.key_codes.RIGHT)) {
-
-    // Incremento l’indice e lo faccio tornare a 0 se supera 5
-    bg_index = (bg_index + 1);
-
-    // Mostro il nuovo sfondo
-    current_bg = PP.assets.image.add(s, sfondi[bg_index], 0, 0, 0, 0);
+    
   }
 }
 
