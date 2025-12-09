01649f46d13bfad8d29d554be0f65774fc8e2637
Mattia Lampugnani <mattiacesare.lampugnani@mail.polimi.it>
2025-12-09 12:53:30 +0100

    primo pezzo doppio salto

--- src/player.js
+++ src/player.js
@@ -2,6 +2,9 @@
 
 let player_speed = 300;
 let jump_init_speed = 700;
+
+let space_pressed = false;
+let mid_jump = true;
 // Variabile obsoleta: floor_height = 620;
 
 let curr_anim = "stop";
@@ -21,9 +24,6 @@ function configure_player_animations(s, player) {
     //Per evitare incastri con hitbox godot
     player.ph_obj.body.setSize(player.ph_obj.width - 8, player.ph_obj.height - 8, true);
 
-    // CORREZIONE SCALA
-    player.geometry.scale_x = 1;
-    player.geometry.scale_y = 1;
 
     // HITBOX 
 
@@ -63,27 +63,20 @@ function manage_player_update(s, player) {
     // 1. Controlla se il corpo del player è bloccato verso il basso da un altro oggetto (i muri di Godot)
     let is_on_solid_ground = player.ph_obj.body.blocked.down;
 
-    let double_jump = true
-    let second_jump = false
-
 
     // 2. Esegue il salto solo se il player è a terra
+
     if (is_on_solid_ground) {
-        double_jump = true;
-        second_jump = false
-        if (PP.interactive.kb.is_key_down(s, PP.key_codes.SPACE)) {
+
+        if (PP.interactive.kb.is_key_down(s, PP.key_codes.SPACE) && space_pressed == false) {
+            space_pressed = true;
             PP.physics.set_velocity_y(player, -jump_init_speed);
-            double_jump = false;
-            second_jump = true;
-            if (PP.interactive.kb.is_key_up (s, PP.key_codes.SPACE)) {
-                if (PP.interactive.kb.is_key_down (s, PP.key_codes.SPACE)) {
-                    PP.physics.set_velocity_y(player, -jump_init_speed);
-                    double_jump = false;
-                    second_jump = false;
-                }
-            }
             // Se a terra e fermo (o in corsa), resetta l'animazione di caduta/salto
         }
+        if (PP.interactive.kb.is_key_up(s, PP.key_codes.SPACE) && space_pressed == true){
+            space_pressed = false;
+        }
+        mid_jump = true;
     }
     // if (is_on_air) {
     //     if (PP.interactive.kb.is_key_down(s, PP.key_codes.SPACE)) {
