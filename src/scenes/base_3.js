1a897239017ecfb9e8e247d5b5c9c0d32a2a5d63
Edoardo Cicerone <edoardofederico.cicerone@polimi.it>
2026-01-30 06:45:28 +0100

    Sfondi lvl2

--- src/scenes/base_3.js
+++ src/scenes/base_3.js
@@ -5,6 +5,8 @@ let muri_livello;
 // Sfondo Parallasse
 let parallasse1; let parallasse2;
 let ts_background_1; let ts_background_2;
+let parallasse_nuvole; 
+let ts_nuvole;
 let lista_trappole = [] // Lava/Sabbie
 
 // Trigger di fine livello
@@ -66,7 +68,8 @@ function preload(s) {
     // Player e Sfondo
     img_player = PP.assets.sprite.load_spritesheet(s, "assets/images/PLAYER/sparo 52x52.png", 52, 52)
     parallasse1 = PP.assets.image.load(s, "assets/images/parallax/parallasse_1.png")
-    parallasse2 = PP.assets.image.load(s, "assets/images/parallax/parallasse_2b.png")
+    parallasse2 = PP.assets.image.load(s, "assets/images/parallax/parallasse_2.png")
+    parallasse_nuvole = PP.assets.image.load(s, "assets/images/parallax/nuvole.png");
 
     // Mappa Godot
     if (window.godot_preload) window.godot_preload(s)
@@ -166,15 +169,20 @@ function create(s) {
     const PARALLAX_WIDTH = 15800
     const PARALLAX_HEIGHT = 3000
 
-    ts_background_1 = PP.assets.tilesprite.add(s, parallasse1, 0, 450, PARALLAX_WIDTH, PARALLAX_HEIGHT, 0, 0.5)
+    ts_nuvole = PP.assets.tilesprite.add(s, parallasse_nuvole, 0, -90, PARALLAX_WIDTH, PARALLAX_HEIGHT, 0, 0.5)
+    ts_nuvole.geometry.scale_x = 0.5; ts_nuvole.geometry.scale_y = 0.5
+
+    ts_background_1 = PP.assets.tilesprite.add(s, parallasse1, 0, 500, PARALLAX_WIDTH, PARALLAX_HEIGHT, 0, 0.5)
     ts_background_1.geometry.scale_x = 0.6; ts_background_1.geometry.scale_y = 0.6
 
-    ts_background_2 = PP.assets.tilesprite.add(s, parallasse2, 0, 450, PARALLAX_WIDTH, PARALLAX_HEIGHT, 0, 0.5)
+    ts_background_2 = PP.assets.tilesprite.add(s, parallasse2, 0, 550, PARALLAX_WIDTH, PARALLAX_HEIGHT, 0, 0.5)
     ts_background_2.geometry.scale_x = 0.6; ts_background_2.geometry.scale_y = 0.6
 
+    
     // [PHASER] Scroll factor 0 per muoverlo manualmente
     ts_background_1.tile_geometry.scroll_factor_x = 0
     ts_background_2.tile_geometry.scroll_factor_x = 0
+    ts_nuvole.tile_geometry.scroll_factor_x = 0
 
     // MAPPA GODOT
     if (window.godot_create && typeof LIV3 !== 'undefined') {
@@ -367,6 +375,8 @@ function update(s) {
     ts_background_2.tile_geometry.x = PP.camera.get_scroll_x(s) * 0.4
     ts_background_1.tile_geometry.y = PP.camera.get_scroll_y(s) * -0.1
     ts_background_2.tile_geometry.y = PP.camera.get_scroll_y(s) * -0.2
+    ts_nuvole.tile_geometry.x = PP.camera.get_scroll_x(s) * 0.5;
+    ts_nuvole.tile_geometry.y = PP.camera.get_scroll_y(s) * -0.03;
 
     // FINE LIVELLO (Controllo Vittoria)
     if (!fine_livello_triggerata && player.ph_obj.x >= X_FINE_LIVELLO) {
